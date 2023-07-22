68e82eba87076d66457dc939bd9ac25e774d89e7
GabrielBetini <contatobetini@outlook.com>
2023-07-22 17:05:35 -0300

    Simple scheme color add

---
+++ code.js
@@ -0,0 +1,12 @@
+window.onload = init;
+
+function init(){
+const colors = ["green", "blue", "rgba(53, 176, 247, 0.9)", "white", "#334522"]
+const button = document.getElementById("btn")
+const color = document.getElementById("color")
+
+button.addEventListener("click", function (){
+    const randomNumber = Math.floor(Math.random() * colors.length);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber]
+})}
